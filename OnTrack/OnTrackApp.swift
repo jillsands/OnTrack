68cbf88c7c4949215bcaaa83c32f89ed32b932ca
Jillian Sands <jesands21@gmail.com>
2022-07-19 22:08:55 -0400

    Initial Commit

---
+++ OnTrack/OnTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnTrackApp.swift
+//  OnTrack
+//
+//  Created by Jillian Sands on 7/19/22.
+//
+
+import SwiftUI
+
+@main
+struct OnTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
